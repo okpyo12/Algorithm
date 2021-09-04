85569b8d1a30701eaac076279023fd100acfc445
okpyo12 <psw3619@naver.com>
2021-09-04 18:24:14 +0900

    추가: Baekjoon_4949 문제 풀이 추가

---
+++ Baekjoon Algorithm/CLASS 2/Baekjoon_4949/Baekjoon_4949.py
@@ -0,0 +1,14 @@
+a = ''
+result = []
+while True:
+    stack = []
+    arr = []
+    TorF = []
+    a = input()
+    if a == '.':
+        break
+    for i in a:
+        arr.append(i)
+
+for i in result:
+    print(i)
