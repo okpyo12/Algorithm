9df2ac0e91ef68a36e8077498a3d5634b901ccd5
okpyo12 <psw3619@naver.com>
2021-09-05 09:04:31 +0900

    수정: Baekjoon_4949 문제 풀이 수정

--- Baekjoon Algorithm/CLASS 2/Baekjoon_4949/Baekjoon_4949.py
+++ Baekjoon Algorithm/CLASS 2/Baekjoon_4949/Baekjoon_4949.py
@@ -3,12 +3,28 @@ result = []
 while True:
     stack = []
     arr = []
-    TorF = []
     a = input()
     if a == '.':
         break
     for i in a:
         arr.append(i)
+    for i in arr:
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ')':
+            if len(stack) != 0 and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(i)
+        elif i == ']':
+            if len(stack) != 0 and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(i)
+    if len(stack) == 0:
+        result.append('yes')
+    else:
+        result.append('no')
 
 for i in result:
     print(i)
