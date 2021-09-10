8ffb0acfc631c9bc6d0b939aa5578ca1fff884b4
okpyo12 <psw3619@naver.com>
2021-09-10 19:57:08 +0900

    추가: Baekjoon_11651 문제 풀이 추가

---
+++ Baekjoon Algorithm/CLASS 2/Baekjoon_11651/Baekjoon_11651.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.stdin.readline())
+arr = []
+for i in range(N):
+    x, y = map(int, sys.stdin.readline().split())
+
+    arr.append([x,y])
+arr = sorted(arr, key=lambda x : (x[1],x[0]))
+
+for i in range(N):
+    print(arr[i][0], arr[i][1])
