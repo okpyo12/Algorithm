edd9bcdec79091a798632a43a29df354e04bed68
okpyo12 <psw3619@naver.com>
2021-09-13 15:49:00 +0900

    추가: Baekjoon_1012 문제 풀이 추가

--- Baekjoon Algorithm/CLASS 3/Baekjoon_1012/Baekjoon_1012.py
+++ Baekjoon Algorithm/CLASS 3/Baekjoon_1012/Baekjoon_1012.py
@@ -1,1 +1,11 @@
-print(1)
+import sys
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    M, N, K = map(int, sys.stdin.readline().split())
+    arr = []
+    for j in range(K):
+        X, Y = map(int, sys.stdin.readline().split())
+        arr.append([X,Y])
+    print(arr)
