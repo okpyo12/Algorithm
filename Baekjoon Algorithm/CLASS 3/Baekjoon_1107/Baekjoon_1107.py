3fd4476ed94ee215e52f1d34639caf55acf0d7ae
Pyosangwoo <psw3619@naver.com>
2021-09-18 18:31:31 +0900

    추가: Baekjoon_1107 문제 풀이 추가

---
+++ Baekjoon Algorithm/CLASS 3/Baekjoon_1107/Baekjoon_1107.py
@@ -0,0 +1,27 @@
+import sys
+
+N = str(sys.stdin.readline())
+M = int(sys.stdin.readline())
+arr = list(map(str, sys.stdin.readline().split()))
+
+num = ['0','1','2','3','4','5','6','7','8','9']
+channel = 100
+cnt = 0
+for i in range(len(num)-len(arr)):
+    for j in range(len(arr)):
+        if num[i] == arr[j]:
+            del num[i]
+print(num)
+
+"""a = ''
+for i in N:
+    if i in num:
+        cnt += 1
+        a += i
+    else:
+        temp = []
+        for j in num:
+            temp.append()
+        a += max(temp)"""
+
+print(cnt)
