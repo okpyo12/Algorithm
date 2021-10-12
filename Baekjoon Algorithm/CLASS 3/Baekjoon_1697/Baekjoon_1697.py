d694341c91d6f50a4b06d09cfd1ea9ef1e2d93ad
Pyosangwoo <psw3619@naver.com>
2021-10-12 14:19:26 +0900

    추가: 문제 풀이 추가

---
+++ Baekjoon Algorithm/CLASS 3/Baekjoon_1697/Baekjoon_1697.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline()
+N,K = map(int,input.split())
+MAX = 100000
+dist = [0] * (MAX+1)
+
+def bfs():
+    q = deque()
+    q.append(N)
+    while q:
+        x = q.popleft()
+        if x == K:
+            print(dist[x])
+            break
+        for j in (x-1,x+1,x*2):
+            if 0<= j <= MAX and not dist[j]:
+                dist[j] = dist[x] +1
+                q.append(j)
+
+bfs()
