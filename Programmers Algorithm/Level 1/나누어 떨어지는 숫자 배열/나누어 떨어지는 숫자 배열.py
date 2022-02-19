1d7a6c6a1f35c9d09e8579526fc0fc27f7ff1085
Pyosangwoo <psw3619@naver.com>
2022-02-19 13:41:32 +0900

    문제 풀이 추가

---
+++ Programmers Algorithm/Level 1/나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -0,0 +1,11 @@
+def solution(arr, divisor):
+    answer = []
+    for i in arr:
+        if i % divisor == 0:
+            answer.append(i)
+    answer.sort()
+    if len(answer) == 0:
+        answer.append(-1)
+    return answer
+
+#def solution(arr, divisor): return sorted([n for n in arr if n%divisor == 0]) or [-1]
