7d02626d086154f46355737be894fe73e1e85fd0
Pyosangwoo <psw3619@naver.com>
2022-03-07 10:37:43 +0900

    문제 풀이 추가

---
+++ Baekjoon Algorithm/CLASS 3/Baekjoon_2579/Baekjoon_2579.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+s = [0 for _ in range(301)]
+dp = [0 for _ in range(301)]
+for i in range(n):
+    s[i] = int(sys.stdin.readline())
+dp[0] = s[0]
+dp[1] = s[0] + s[1]
+dp[2] = max(s[1] + s[2], s[0] + s[2])
+for i in range(3, n):
+    dp[i] = max(dp[i - 3] + s[i - 1] + s[i], dp[i - 2] + s[i])
+print(dp[n - 1])
