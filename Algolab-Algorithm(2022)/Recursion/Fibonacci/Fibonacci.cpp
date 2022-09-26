696c6a369564a3d1a16f4084531c6ef6b6009004
Pyosangwoo <psw3619@naver.com>
2022-09-26 23:17:07 +0900

    Fibonacci 문제 해결

---
+++ Algolab-Algorithm(2022)/Recursion/Fibonacci/Fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int fibonacci(int n);
+int main()
+{
+    int numTestCases;
+    scanf("%d", &numTestCases);
+    for (int i = 0; i < numTestCases; ++i)
+    {
+        int num;
+        scanf("%d", &num);
+        printf("%d", fibonacci(num));
+        printf("\n");
+    }
+}
+
+int fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
