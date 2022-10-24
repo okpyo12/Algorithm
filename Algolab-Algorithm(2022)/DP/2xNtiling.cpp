3a3602835b67699cff401410e68f5ca08ed10020
Pyosangwoo <psw3619@naver.com>
2022-10-25 08:14:21 +0900

    Create 2xNtiling.cpp

---
+++ Algolab-Algorithm(2022)/DP/2xNtiling.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int arr[1000];
+	arr[0] = 1;
+	arr[1] = 2;
+	for (int i = 2; i < n; i++)
+	{
+		arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+	}
+	cout << arr[n - 1];
+	return 0;
+}
