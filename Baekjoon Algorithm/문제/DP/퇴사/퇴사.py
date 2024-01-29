46c872e98b214f402d15a379aab4d53f1009c6ea
Pyosangwoo <psw3619@naver.com>
2024-01-30 00:21:47 +0900

    Solved DP: 퇴사

---
+++ Baekjoon Algorithm/문제/DP/퇴사/퇴사.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+schedule = [list(map(int, input().split())) for _ in range(N)]
+dp = [0 for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(i+schedule[i][0], N+1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
+
+print(dp[-1])
