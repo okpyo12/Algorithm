f9575a2e4bccc9e2aff45893665c2e8c92f44ae1
Pyosangwoo <psw3619@naver.com>
2024-02-29 11:00:31 +0900

    Solved 수학: 대칭차집합

---
+++ Baekjoon Algorithm/문제/수학/대칭차집합/대칭차집합.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+result = len(A-B) + len(B-A)
+print(result)
