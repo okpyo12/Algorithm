b970b643ff760b6969d9854f480282ae582b2894
Pyosangwoo <psw3619@naver.com>
2024-03-22 13:06:43 +0900

    Solved 그래프: 경로찾기

---
+++ Baekjoon Algorithm/문제/그래프/경로찾기/경로찾기.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            if arr[j][i] and arr[i][k]:
+                arr[j][k] = 1
+                
+for g in arr:
+    print(*g)
