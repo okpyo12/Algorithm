59802ec6bdea2b907fa5d49bf33ec97d758a0f6d
Pyosangwoo <psw3619@naver.com>
2024-03-24 13:53:32 +0900

    Solved DP: 오르막수

---
+++ Baekjoon Algorithm/문제/DP/오르막수/오르막수.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [1]*10
+for i in range(1,N) :
+    for j in range(1,10) :
+        dp[j] += dp[j-1]
+        print(dp)
+
+print(sum(dp)%10007)
