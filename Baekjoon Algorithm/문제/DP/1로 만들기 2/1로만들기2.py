997a5daf878f879e3fb5d03582fa0f12e7c56e3e
Pyosangwoo <psw3619@naver.com>
2024-06-22 12:31:37 +0900

    Solved DP: 1로 만들기 2

---
+++ Baekjoon Algorithm/문제/DP/1로 만들기 2/1로만들기2.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0]*1000001
+
+for i in range(2,N+1):
+    dp[i] = dp[i-1] + 1
+    
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+
+print(dp[N])
+
+res = [N]
+now = N
+temp = dp[N] - 1
+
+for i in range(N, 0, -1):
+    if dp[i] == temp and (i+1 == now or i*2 == now or i*3 == now):
+        now = i
+        res.append(i)
+        temp -= 1
+print(*res)
