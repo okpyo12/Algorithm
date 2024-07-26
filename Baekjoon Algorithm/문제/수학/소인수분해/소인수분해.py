86666e5a07f28dd2365ad9bb6ee4597d411971e1
Pyosangwoo <psw3619@naver.com>
2024-07-27 01:46:33 +0900

    Solved 수학: 소인수분해

---
+++ Baekjoon Algorithm/문제/수학/소인수분해/소인수분해.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+if N == 1:
+    print('')
+
+for i in range(2, N+1):
+    if N % i == 0:
+        while N % i == 0:
+            print(i)
+            N = N / i
