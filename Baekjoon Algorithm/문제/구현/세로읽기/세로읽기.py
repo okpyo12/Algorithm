4a26a9cb69eb9c775dc8b299695f8112b4a159fa
Pyosangwoo <psw3619@naver.com>
2024-07-30 11:19:23 +0900

    Solved 구현: 세로읽기

---
+++ Baekjoon Algorithm/문제/구현/세로읽기/세로읽기.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+arr = [list(input().rstrip()) for _ in range(5)]
+
+for j in range(15):
+    for i in range(5):
+        if j < len(arr[i]):
+            print(arr[i][j], end='')
