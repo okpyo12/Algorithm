13988dd4148554436748109ac89d3d2dc1d4264b
Pyosangwoo <psw3619@naver.com>
2024-08-02 11:59:20 +0900

    Solved 구현: 문서겁색

---
+++ Baekjoon Algorithm/문제/구현/문자열/문서검색/문서검색.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+S = sys.stdin.readline().strip()
+x = sys.stdin.readline().strip()
+i = 0
+cnt = 0
+while i < len(S):
+    if S[i:i+len(x)] == x:
+        i += len(x)
+        cnt += 1
+    else :
+        i += 1
+print(cnt)
