4bdb31c1bc6f75829c0032c909bd4f7c8a57fbb8
Pyosangwoo <psw3619@naver.com>
2024-08-03 11:02:52 +0900

    Sovled 구현: 쉽게푸는문제

---
+++ Baekjoon Algorithm/문제/구현/쉽게푸는문제/쉽게푸는문제.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+a,b = map(int,input().split())
+data = [0]
+sum = 0
+
+for i in range(1,b+1):
+  for j in range(i):
+    data.append(i)
+
+for i in range(a, b+1):
+  sum+=data[i]
+print(sum)
