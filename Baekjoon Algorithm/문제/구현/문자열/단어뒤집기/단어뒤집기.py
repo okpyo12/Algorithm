bf241f7c3b75b8503f303e578d00102ceb93ffbc
Pyosangwoo <psw3619@naver.com>
2024-08-06 22:20:57 +0900

    Solved 구현: 단어뒤집기

---
+++ Baekjoon Algorithm/문제/구현/문자열/단어뒤집기/단어뒤집기.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    str = sys.stdin.readline().rstrip()
+    words = list(str.split())
+    reverse_words = []
+
+    for word in words:
+        reverse_words.append(word[::-1])
+
+    answer = " ".join(reverse_words)
+    print(answer)
