8b60ee408de96cf0a55b92908aea4a39d34d5a24
Pyosangwoo <psw3619@naver.com>
2024-08-17 15:41:35 +0900

    Solved 정렬: 점수계산

---
+++ Baekjoon Algorithm/문제/정렬/점수계산/점수계산.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+score = []
+for i in range(8):
+    score.append(int(input()))
+temp = []
+answer = 0
+for i in range(5):
+    answer += max(score)
+    temp.append(score.index(max(score)) + 1)
+    score[score.index(max(score))] = -1
+temp.sort()
+print(answer)
+print(*temp)
