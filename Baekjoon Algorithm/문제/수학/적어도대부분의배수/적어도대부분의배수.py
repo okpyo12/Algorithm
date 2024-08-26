4383cac8ba43db2d0767f1241addfe7e8b19a9a7
Pyosangwoo <psw3619@naver.com>
2024-08-26 10:40:40 +0900

    Solved 수학: 적어도대부분의배수

---
+++ Baekjoon Algorithm/문제/수학/적어도대부분의배수/적어도대부분의배수.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+arr = list(map(int, input().split()))
+
+minNum = min(arr)
+cnt = 0
+while True:
+    cnt = 0
+    for i in range(len(arr)):
+        if minNum % arr[i] == 0 :
+            cnt += 1
+    if cnt >= 3:
+        print(minNum)
+        break
+    minNum += 1
